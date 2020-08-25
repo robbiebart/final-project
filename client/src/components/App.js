8884324406bf1488aa2b472a6b70a19460d93749
Robbie Bart <adm@AdminisatorsAir.lan>
2020-08-25 19:56:55 -0400

    setting up state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,3 +6,18 @@ function App() {
 }
 
 export default App;
+
+/*
+{
+    "id": "001",
+    "text": "text",
+    "answer-a": "text",
+    "answer-a-id": "001",
+    "answer-b": "text",
+    "answer-b-id": "001",
+    "answer-c": "text",
+    "answer-c-id": "001",
+    "answer-d": "text",
+    "answer-d-id": "001"
+  },
+  */
