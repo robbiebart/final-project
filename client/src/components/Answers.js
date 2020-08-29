b936ed299c15f2fcb8aceac1c1d181e2e8b8b391
Robbie Bart <adm@AdminisatorsAir.lan>
2020-08-29 18:39:27 -0400

    typo in last push

--- client/src/components/Answers.js
+++ client/src/components/Answers.js
@@ -6,29 +6,6 @@ export default function Answers({ answers }) {
   return answers.map((answer) => {
     const a = questions.find((q) => q.id === answer.parent);
     return (
-      <AnsComponent>
-        {a.text}
-        <br />
-        {answer.label}
-      </AnsComponent>
-    );
-  });
-}
-
-const AnsComponent = styled.div`
-  grid-column-start: main-start;
-  grid-column-end: main-end;
-
-  grid-row-start: fourth-line;
-`;
-import React from "react";
-import styled from "styled-components";
-const questions = require("../QuestionData.json");
-
-export default function Answers({ answers }) {
-  return answers.map((answer) => {
-    const a = questions.find((q) => q.id === answer.parent);
-    return (
       <AnsComponent key={answer.value}>
         {a.text}
         <br />
